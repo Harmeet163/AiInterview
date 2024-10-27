cd6b71858ea367e06b4c8f2ed751a10a9e9c71ad
Harmeet163 <harmeetchavda163@gmail.com>
2024-10-27 22:13:26 +0530

    data show in dashboard

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,18 +1,20 @@
-import React from 'react'
-import AddNewInterview from './_component/AddNewInterview'
+import React from "react";
+import AddNewInterview from "./_component/AddNewInterview";
+import PreviousInterview from "./_component/PreviousInterview";
 
 const page = () => {
   return (
-    <div className='p-10'>
-    <h2 className='font-bold text-2xl'>Dashboard</h2>
-    <h2 className='text-gray-500'>Create and Start your interviw</h2>
+    <div className="p-10">
+      <h2 className="font-bold text-2xl">Dashboard</h2>
+      <h2 className="text-gray-500">Create and Start your interviw</h2>
 
+      <div className="grid grid-cols-1 md:grid-cols-3 my-5">
+        <AddNewInterview />
+      </div>
 
-    <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
-      <AddNewInterview/>
+      <PreviousInterview />
     </div>
-    </div>
-  )
-}
+  );
+};
 
-export default page
+export default page;
